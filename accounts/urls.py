4323e016ad8e94203688c31347f99072f39dab0d
nexus <ssiban695@gmail.com>
2025-02-05 07:48:42 +0000

    url fixed

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .views import AppleOauthView
 
 urlpatterns = [
-    path('oauth/apple/callback', AppleOauthView.as_view(), name = "apple_callback"),
+    path('oauth/apple/callback/', AppleOauthView.as_view(), name = "apple_callback"),
 ]
